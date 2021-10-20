d52c9ee694504a39269714c5fd15c15c0e08eada
Josh Willis <joshuaw1998@gmail.com>
2021-10-20 17:47:58 -0600

    unique string added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.nio.charset.Charset;
+import java.util.Random;
 
 class Calculator {
 
@@ -74,8 +76,23 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(n);
 
-        return null;
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        stringBuilder.append(generatedString);
+
+        return stringBuilder.toString();
     }
 
 
